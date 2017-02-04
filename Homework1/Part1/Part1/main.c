f9b05f72a520949744e2e33d1584756ab3b535e7
jr2339 <jr2339@nau.edu>
2017-02-03 21:02:45 -0700

    have segment fault

--- Homework1/Part1/Part1/main.c
+++ Homework1/Part1/Part1/main.c
@@ -10,11 +10,55 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#define MAX_SIZE 2000000000
+
+// Function at here to Calcuate the total size we need to reserved.
+//At here, i will give a little bigger than we calculated
+int count_size(const char* filename){
+    int size;
+    int col = 0;
+    int line = 0;
+    
+    
+    
+    FILE *fp = fopen(filename, "r");
+    
+    if (!fp) {
+        fprintf(stderr, "can't open file for reading\n");
+        exit(1);
+    }
+    
+    while(1){
+        
+        int ch = getc(fp);
+        while (ch == '>') {
+            do{
+                ch = getc(fp);
+            }
+            while (ch!='\n');
+            ch = getc(fp);
+        }
+        
+        if (feof(fp)) {
+            break;
+        }
+       
+        col++;
+        if (ch == '\n') {
+            line++;
+        }
+    }
+    size = (line +1)*col;
+    return size;
+    
+};
 
 
-void read_save_file(const char* filename,char* buffer,char* result){
+
+
+
+int skip_header(const char* filename,char* buffer){
     int count = 0;
+    int line = 0;
     
   
     
@@ -42,26 +86,51 @@ void read_save_file(const char* filename,char* buffer,char* result){
         //save the character to buffer
         buffer[count] = ch;
         count++;
+        if (ch == '\n') {
+            line++;
+        }
     }
 
-
     
+    printf("line is %d\n",line);
     
     fclose(fp);
+    return line;
    
 }
 
 
-void sort_file(const char* filename,char* buffer,char* result){
+
+void merge_sort(const char* filename,char* buffer,char** result){
+    int line = skip_header(filename,buffer);
+    char** left;
+    char** right;
+    printf("HAHAHAHAHAHAHAHAHA\n");
+    for (int i=0; i<line/2+1; i++) {
+        **left = **result;
+        left++;
+        result++;
+    }
+     printf("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\n");
+    for (int j=0; j<line/2; j++) {
+        right[j] = result[line/2+j+1];
+    }
     
-    read_save_file(filename,buffer,result);
-    
-    printf("%s\n",result);
+    printf("%s\n",*left);
     
     printf("=============================================================\n");
+    printf("%s\n",*right);
     
-    printf("%s\n",result);
     printf("=============================================================\n");
+}
+
+void sort_file(const char* filename,char* buffer,char** result){
+    
+   
+    merge_sort(filename,buffer,result);
+
+    
+
     
     
     
@@ -80,12 +149,23 @@ int main(int argc, const char * argv[]) {
         perror("Too many arguments");
         exit(1);
     }
+   
     const char *input = argv[1]; // input file
-    char* buffer = (char*)malloc(MAX_SIZE*sizeof(char));
-    char* result;
-    result = buffer;
+    
+    int size = count_size(input);
+    
+    char* buffer = (char*)malloc(size*sizeof(char));
+    
+    char** result;
+    result = &buffer;
 
     sort_file(input,buffer,result);
+    
+    //printf("%s\n",*result);
+    
+    //printf("=============================================================\n");
+    
+
     
     free(buffer);
     
