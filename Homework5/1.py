13d2fe6a67722a58cec9fe054b841808b6154195
jr2339 <jr2339@nau.edu>
2017-03-22 22:57:27 -0700

    Finished part1

--- Homework5/1.py
+++ Homework5/1.py
@@ -1,53 +1,49 @@
 import random
-
-def get_sequence(length):
-    DNA_Sequence = ""
-    for i in range(length):
-        DNA_Sequence += random.choice("ACGT")
-        return DNA_Sequence
+import numpy
+def Simulation(length,size):
 
 
-def get_success(length):
-    success = []
-    for i in range(length):
-        random_number = random.randint(1,100)
-        if random_number == 10:
-            success.append(True)
-        else:
-            success.append(False)
-    return success
-
-
-
-def Sequence(length):
-    Original_Sequence = get_sequence(length)
-    squence = ""
-    success = get_success(length)
-    for i in range(length):
-        if success[i] == True:
-            squence += Original_Sequence[i]
-        else:
-            if Original_Sequence[i] == 'A':
-                squence += random.choice("CGT")
-            elif Original_Sequence[i] == 'C':
-                squence += random.choice("AGT")
-            elif Original_Sequence[i] == 'G':
-                squence += random.choice("ACT")
-            else:
-                squence += random.choice("ACG")
-    return squence
-
-
-def count(length,size):
+    #calculate the match
     counter = []
-    Original_Sequence = get_sequence(length)
-    sequence = Sequence(length)
-    for i in range(100):
-        coun = 0
-        for j in range(length - size):
-            if Original_Sequence[j:j+size-1] == sequence[j:j+size-1]:
-                coun = coun + 1
-        counter.append(coun)
-    return counter
 
-count(36,30)
+
+    for i in range(100000):
+        # Generate Original Sequence
+        Original_Sequence = ""
+        for i in range(length):
+            Original_Sequence += random.choice("ACGT")
+
+        # calculate the 1% error
+        success = []
+        for i in range(length):
+            random_number = random.randint(1, 100)
+            if random_number == 10:
+                success.append(False)
+            else:
+                success.append(True)
+
+        # calculate the error Sequence
+        Finial_Squence = ""
+        for i in range(length):
+            if success[i] == True:
+                Finial_Squence += Original_Sequence[i]
+            elif success[i] == False:
+                if Original_Sequence[i] == 'A':
+                    Finial_Squence += random.choice("CGT")
+                elif Original_Sequence[i] == 'C':
+                    Finial_Squence += random.choice("AGT")
+                elif Original_Sequence[i] == 'G':
+                    Finial_Squence += random.choice("ACT")
+                else:
+                    Finial_Squence += random.choice("ACG")
+
+        coun = 0
+        for j in range(length - size+1):
+            if Original_Sequence[j:j+size-1] == Finial_Squence[j:j+size-1]:
+                coun = coun + 1
+
+        counter.append(coun)
+    average = numpy.mean(counter)
+    return average
+
+print(Simulation(36,30))
